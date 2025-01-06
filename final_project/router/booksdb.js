18bcde0cdbcc149ec298ce53ba99d22c7a8d23c5
Sampath <ichinthaka333@gmail.com>
2025-01-06 03:02:07 -0500

    not finish book retrive isbn commit

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"isbn":"100","author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
+      2: {"isbn":"200","author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
+      3: {"isbn":"300","author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
+      4: {"isbn":"400","author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
+      5: {"isbn":"500","author": "Unknown","title": "The Book Of Job", "reviews": {} },
+      6: {"isbn":"600","author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
+      7: {"isbn":"700","author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
+      8: {"isbn":"800","author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
+      9: {"isbn":"900","author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
+      10: {"isbn":"1000","author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
 }
 
 module.exports=books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,16 +29,23 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  res.send(books,null,4);
-  //res.send(JSON.stringify(books,null,4));
+  //res.send(books,null,4);
+  res.send(JSON.stringify(books,null,4));
   //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
+  //public_users.get('/isbn/:isbn',function (req, res) {
+    //Write your code here
   //res.send(JSON.stringify(books,null,4));
-  return res.status(300).json({message: "Yet to be implemented"});
+  //res.send(JSON.stringify(books,null,4));
+  const isbn = req.params.isbn;
+  //res.send(books[isbn]);
+  //res.send(JSON.stringify(books[isbn],null,4));
+  res.send(isbn,null,4);
+
+  //return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
